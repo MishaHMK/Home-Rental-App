01d18abcd9e9fa2e8985a29400e99ed0b62cc33e
MishaHMK <gumeniukmich@gmail.com>
2025-04-17 20:34:44 +0300

    fix 2

--- src/main/java/rental/project/dto/payment/CreatePaymentDto.java
+++ src/main/java/rental/project/dto/payment/CreatePaymentDto.java
@@ -1,8 +1,10 @@
 package rental.project.dto.payment;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreatePaymentDto {
+    @NotNull(message = "Booking id is required")
     private Long bookingId;
 }
